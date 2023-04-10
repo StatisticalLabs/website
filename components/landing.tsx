12e37ef3dde217ceb6158093de5863622a4d0073
ToastedToast <theabcashow.josh@gmail.com>
2023-04-10 15:46:48 +0800

    better responsiveness

--- components/landing.tsx
+++ components/landing.tsx
@@ -4,19 +4,19 @@ import type React from "react";
 const Landing: React.FC = () => {
   return (
     <div className="py-8 px-6">
-      <div className="flex items-center gap-3 jsutify-between">
-        <div>
+      <div className="flex flex-col lg:flex-row items-center gap-3 justify-between">
+        <div className="text-center lg:text-left">
           <h1 className="font-bold text-5xl">
             The <span className="font-normal italic underline">best</span>{" "}
             tracking bot on{" "}
-            <span className="bg-gradient-to-b from-blue-600 to-indigo-400 inline-block text-transparent bg-clip-text">
+            <span className="bg-gradient-to-b from-blue-800 to-indigo-600 dark:from-blue-600 dark:to-indigo-400 inline-block text-transparent bg-clip-text">
               Discord
             </span>
             .
           </h1>
-          <p className="mt-2">
+          <p className="mt-4">
             Track any{" "}
-            <span className="bg-gradient-to-b from-red-400 to-red-600 inline-block text-transparent bg-clip-text font-medium">
+            <span className="bg-gradient-to-b from-red-600 to-red-800 dark:from-red-400 dark:to-red-600 inline-block text-transparent bg-clip-text font-medium">
               YouTube channel
             </span>{" "}
             and immediately find out when they have a subscriber update.
