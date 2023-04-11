41dcb809b9120168089048a3f0826ff7b7bef4d3
Not TCA <realtcatech@gmail.com>
2023-04-11 14:55:49 +0800

    fix httpEquiv warning

--- theme.config.tsx
+++ theme.config.tsx
@@ -53,7 +53,7 @@ const config: DocsThemeConfig = {
     return (
       <>
         <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="title" content={titleTemplate.replace("%s", title)} />
         <meta name="description" content={description} />
