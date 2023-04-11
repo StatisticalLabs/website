b01ea4e0c3e5fb0c9ac370e1d3ae2129c6a4d5f0
Not TCA <realtcatech@gmail.com>
2023-04-11 13:56:56 +0800

    add redirects, update navbar links

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  redirects: async () => [
+    {
+      source: "/discord",
+      destination: "https://discord.gg/MEF8q6MwTc",
+      permanent: true,
+    },
+    {
+      source: "/invite",
+      destination:
+        "https://discord.com/api/oauth2/authorize?client_id=1077159995487035402&permissions=19456&scope=applications.commands%20bot",
+      permanent: false,
+    },
+  ],
+});
