a915b082c95655d280b5ea87a9761c03ac02480f
Not TCA <realtcatech@gmail.com>
2023-04-11 14:36:41 +0800

    more global titles

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { useConfig, type DocsThemeConfig } from "nextra-theme-docs";
 
+const titleTemplate = "%s – Statistical";
+
 const config: DocsThemeConfig = {
   logo: (
     <span>
@@ -15,7 +17,7 @@ const config: DocsThemeConfig = {
       />
     </span>
   ),
-  useNextSeoProps: () => ({ titleTemplate: "%s – Statistical" }),
+  useNextSeoProps: () => ({ titleTemplate }),
   project: {
     icon: (
       <svg
@@ -53,7 +55,7 @@ const config: DocsThemeConfig = {
         <meta charSet="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="title" content={title} />
+        <meta name="title" content={titleTemplate.replace("%s", title)} />
         <meta name="description" content={description} />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content={title} />
