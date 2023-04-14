fdb34f97c481a610733a93fd413e8a5fabe4ed81
Not TCA <realtcatech@gmail.com>
2023-04-14 06:50:04 +0000

    fix redirects

--- next.config.mjs
+++ next.config.mjs
@@ -10,15 +10,18 @@ export default withNextra({
     {
       source: "/discord",
       destination: "https://discord.gg/MEF8q6MwTc",
+      permanent: false,
     },
     {
       source: "/invite",
       destination:
         "https://discord.com/api/oauth2/authorize?client_id=1077159995487035402&permissions=19456&scope=applications.commands%20bot",
+      permanent: false,
     },
     {
       source: "/vote",
       destination: "https://top.gg/bot/1077159995487035402/invite",
+      permanent: false,
     },
   ],
   images: {
